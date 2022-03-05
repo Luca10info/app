9e3a6da9824c1e3c575979631332cbdf5f670314
Luca10 <brugnettiluca@gmail.com>
2022-03-05 11:13:49 +0100

    update navbar

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -6,25 +6,18 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import {red} from '@mui/material/colors'
 function Navbar() {
+
+    const color=red[500];
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+    <Box  sx={{ flexGrow: 1 }}>
+      <AppBar style={{backgroundColor: color}} position="static">
+        <Toolbar >
+          <Typography  variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Application
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
